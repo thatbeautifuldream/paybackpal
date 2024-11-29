3316e0f9398f0e978ff7f5eed024db5c315b2d4b
Milind Mishra <milind.mishra4@gmail.com>
2024-11-30 01:00:53 +0530

    feat: update colors and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import DebtorList from "@/components/debtor-list";
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Money Reminder App</h1>
+      <h1 className="text-2xl font-bold mb-4">PaybackPal</h1>
       <AddDebtorForm />
       <DebtorList />
     </div>
